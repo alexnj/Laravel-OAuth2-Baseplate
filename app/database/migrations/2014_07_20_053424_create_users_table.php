a0cf3c794132d9b9045b21e0d8c4f1871d8e0923
Alex Jose <alexnj@gmail.com>
2014-07-19 23:51:00 -0700

    Converts tab based indents to 4-spaces.

--- app/database/migrations/2014_07_20_053424_create_users_table.php
+++ app/database/migrations/2014_07_20_053424_create_users_table.php
@@ -5,31 +5,31 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateUsersTable extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('users', function(Blueprint $t)
-		{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function(Blueprint $t)
+        {
             $t->char('id', 36)->primary()->increments();
             $t->enum('active', array('A','P','D'));
             $t->string('email');
             $t->string('password');
             $t->timestamps();
-		});
-	}
+        });
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('users');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
 
 }
