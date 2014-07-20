4e865ce97c8ce8b034f0fd62998e0876063b26d8
Alex Jose <alexnj@gmail.com>
2014-07-19 23:34:27 -0700

    Adds a User table migration.

---
+++ app/database/migrations/2014_07_20_053424_create_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function(Blueprint $t)
+		{
+            $t->char('id', 36)->primary()->increments();
+            $t->enum('active', array('A','P','D'));
+            $t->string('email');
+            $t->string('password');
+            $t->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}
